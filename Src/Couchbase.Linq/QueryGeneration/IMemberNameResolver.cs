9b3acb36046edb3f55279bb37bdb3365d2a2a3a1
Keith Raven <keith@keth.co.uk>
2014-11-09 19:47:24 +0000

    added json.net member name resolution to where and select clauses

---
+++ Src/Couchbase.Linq/QueryGeneration/IMemberNameResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couchbase.Linq.QueryGeneration
+{
+    public interface IMemberNameResolver
+    {
+        string ResolveMemberName(MemberInfo member);
+    }
+}
