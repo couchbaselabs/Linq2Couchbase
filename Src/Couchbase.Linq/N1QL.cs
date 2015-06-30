7fa4161b444c252a066c9c665d71f26b14573f0d
Brant Burnett <bburnett@centeredgesoftware.com>
2015-06-30 15:23:52 -0400

    Support for IS MISSING and IS VALUED operators
    Motivation
    ----------
    The current .WhereMissing() implementation is limited to the where clause,
    and only does simple filtering logic.  It might be desired to do more
    advanced operations with the IS MISSING and IS VALUED operators, such as
    using them in CASE statements.
    
    Modifications
    -------------
    Added IsMissing, IsNotMissing, IsValued, and IsNotValued static methods to
    the N1Ql class.  Added method call translators for these methods.
    
    Results
    -------
    It is now possible to generate IS MISSING, IS NOT MISSING, IS VALUED, and
    IS NOT VALUED clauses in any expression in the query.  Using the
    "propertyName" parameter string allows testing a property that doesn't
    exist in the .Net object model.

--- Src/Couchbase.Linq/N1QL.cs
+++ Src/Couchbase.Linq/N1QL.cs
@@ -1,4 +1,5 @@
-﻿using Couchbase.Linq.Metadata;
+﻿using System.Linq.Expressions;
+using Couchbase.Linq.Metadata;
 
 namespace Couchbase.Linq
 {
@@ -30,6 +31,160 @@ namespace Couchbase.Linq
                 return null;
             }
         }
+
+        #region IsMissing and IsNotMissing
+
+        /// <summary>
+        /// Returns true if the selected property is missing from the document
+        /// </summary>
+        /// <typeparam name="T">Type of the property being selected</typeparam>
+        /// <param name="property">Property to test</param>
+        /// <returns>True if the property is missing from the document</returns>
+        public static bool IsMissing<T>(T property)
+        {
+            // Implementation will only be called when unit testing
+            // Since properties cannot be missing on in-memory objects
+            // Always returns false
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if the named property is missing from the document
+        /// </summary>
+        /// <typeparam name="T">Type of the document being tested</typeparam>
+        /// <param name="document">Document being tested</param>
+        /// <param name="propertyName">Property name to test</param>
+        /// <returns>True if the property is missing from the document</returns>
+        /// <remarks><see cref="propertyName">propertyName</see> must be a constant when used in a LINQ expression</remarks>
+        public static bool IsMissing<T>(T document, string propertyName)
+        {
+            // Implementation will only be called when unit testing
+            // Test to see if the property is present via reflection
+
+            return (document == null) || (typeof (T).GetProperty(propertyName) == null);
+        }
+
+        /// <summary>
+        /// Returns true if the selected property is present on the document
+        /// </summary>
+        /// <typeparam name="T">Type of the property being selected</typeparam>
+        /// <param name="property">Property to test</param>
+        /// <returns>True if the property is present on the document</returns>
+        public static bool IsNotMissing<T>(T property)
+        {
+            // Implementation will only be called when unit testing
+            // Since properties cannot be missing on in-memory objects
+            // Always returns true
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns true if the named property is present on the document
+        /// </summary>
+        /// <typeparam name="T">Type of the document being tested</typeparam>
+        /// <param name="document">Document being tested</param>
+        /// <param name="propertyName">Property name to test</param>
+        /// <returns>True if the property is present on the document</returns>
+        /// <remarks><see cref="propertyName">propertyName</see> must be a constant when used in a LINQ expression</remarks>
+        public static bool IsNotMissing<T>(T document, string propertyName)
+        {
+            // Implementation will only be called when unit testing
+            // Test to see if the property is present via reflection
+
+            return (document != null) && (typeof(T).GetProperty(propertyName) != null);
+        }
+
+        #endregion
+
+        #region IsValued and IsNotValued
+
+        /// <summary>
+        /// Returns true if the selected property is present on the document and not null
+        /// </summary>
+        /// <typeparam name="T">Type of the property being selected</typeparam>
+        /// <param name="property">Property to test</param>
+        /// <returns>True if the property is present on the document and not null</returns>
+        public static bool IsValued<T>(T property)
+        {
+            // Implementation will only be called when unit testing
+            // Since properties cannot be missing on in-memory objects
+            // Simply test for null
+
+            return property != null;
+        }
+
+        /// <summary>
+        /// Returns true if the named property is not missing from the document and not null
+        /// </summary>
+        /// <typeparam name="T">Type of the document being tested</typeparam>
+        /// <param name="document">Document being tested</param>
+        /// <param name="propertyName">Property name to test</param>
+        /// <returns>True if the property is present on the document and not null</returns>
+        /// <remarks><see cref="propertyName">propertyName</see> must be a constant when used in a LINQ expression</remarks>
+        public static bool IsValued<T>(T document, string propertyName)
+        {
+            // Implementation will only be called when unit testing
+            // Test to see if the property is present and not null via reflection
+
+            if (document == null)
+            {
+                return false;
+            }
+
+            var property = typeof (T).GetProperty(propertyName);
+            if (property == null)
+            {
+                return false;
+            }
+
+            return property.GetValue(document) != null;
+        }
+
+        /// <summary>
+        /// Returns true if the selected property is missing from the document or null
+        /// </summary>
+        /// <typeparam name="T">Type of the property being selected</typeparam>
+        /// <param name="property">Property to test</param>
+        /// <returns>True if the property is missing from the document or null</returns>
+        public static bool IsNotValued<T>(T property)
+        {
+            // Implementation will only be called when unit testing
+            // Since properties cannot be missing on in-memory objects
+            // Simply test for null
+
+            return property == null;
+        }
+
+        /// <summary>
+        /// Returns true if the named property is missing from the document or null
+        /// </summary>
+        /// <typeparam name="T">Type of the document being tested</typeparam>
+        /// <param name="document">Document being tested</param>
+        /// <param name="propertyName">Property name to test</param>
+        /// <returns>True if the property is missing from the document or null</returns>
+        /// <remarks><see cref="propertyName">propertyName</see> must be a constant when used in a LINQ expression</remarks>
+        public static bool IsNotValued<T>(T document, string propertyName)
+        {
+            // Implementation will only be called when unit testing
+            // Test to see if the property is present via reflection
+
+            if (document == null)
+            {
+                return true;
+            }
+
+            var property = typeof(T).GetProperty(propertyName);
+            if (property == null)
+            {
+                return true;
+            }
+
+            return property.GetValue(document) == null;
+        }
+
+        #endregion
 
     }
 }
---
+++ Src/Couchbase.Linq/QueryGeneration/MethodCallTranslators/IsValuedMethodCallTranslator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couchbase.Linq.QueryGeneration.MethodCallTranslators
+{
+    class IsValuedMethodCallTranslator : IMethodCallTranslator
+    {
+        public static readonly MethodInfo[] SupportedMethods =
+            typeof (N1Ql).GetMethods().Where(p => (p.Name == "IsValued") || (p.Name == "IsNotValued")).ToArray();
+            
+        public Expression Translate(MethodCallExpression methodCallExpression, N1QlExpressionTreeVisitor expressionTreeVisitor)
+        {
+            if (methodCallExpression == null)
+            {
+                throw new ArgumentNullException("methodCallExpression");
+            }
+
+            var expression = expressionTreeVisitor.Expression;
+
+            expressionTreeVisitor.VisitExpression(methodCallExpression.Arguments[0]);
+
+            if (methodCallExpression.Arguments.Count > 1)
+            {
+                var constantExpression = methodCallExpression.Arguments[1] as ConstantExpression;
+                if (constantExpression == null)
+                {
+                    throw new NotSupportedException("IsValued and IsNotValued propertyName parameter must be a constant");
+                }
+
+                expression.AppendFormat(".{0}",
+                    N1QlQueryModelVisitor.EscapeIdentifier(constantExpression.Value.ToString()));
+            }
+
+            expression.Append(methodCallExpression.Method.Name == "IsValued" ? " IS VALUED" : " IS NOT VALUED");
+
+            return methodCallExpression;
+        }
+    }
+}
