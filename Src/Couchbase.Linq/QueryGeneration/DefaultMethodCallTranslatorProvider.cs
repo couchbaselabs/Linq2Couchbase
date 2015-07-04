83aaaf3fc7bced3a1d1295cdb8c7a996cea9ed84
Brant Burnett <bburnett@centeredgesoftware.com>
2015-07-04 17:18:21 -0400

    Support for [] array index accessors in expressions
    Modifications
    -------------
    Implemented handling for ExpressionType.ArrayIndex.  Added
    ListIndexMethodCallTranslator to handle IList based collections.
    
    Modified DefaultMethodCallTranslatorProvider to check for method call
    translators on the generic type as well as on any interfaces.  This allows
    ListIndexMethodCallTranslator to simply register against IList<>.get_Item
    instead of every possible class which might implement IList<>.

--- Src/Couchbase.Linq/QueryGeneration/DefaultMethodCallTranslatorProvider.cs
+++ Src/Couchbase.Linq/QueryGeneration/DefaultMethodCallTranslatorProvider.cs
@@ -67,7 +67,139 @@ namespace Couchbase.Linq.QueryGeneration
                 }
             }
 
+            // Check if the generic form of the declaring type matches
+            translator = GetItemFromGenericType(key);
+            if (translator != null)
+            {
+                return translator;
+            }
+            
+            // Finally, check any interfaces that may have a matching 
+            return GetItemFromInterfaces(key);            
+        }
+
+        /// <summary>
+        /// Checks the generic version of the type that implements the key method to see if it has an IMethodCallTranslator defined
+        /// </summary>
+        /// <param name="key">MethodInfo to test</param>
+        /// <returns>Null if no IMethodCallTranslator is found</returns>
+        protected virtual IMethodCallTranslator GetItemFromGenericType(MethodInfo key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            if ((key.DeclaringType == null) ||
+                (!key.DeclaringType.IsGenericType || key.DeclaringType.IsGenericTypeDefinition))
+            {
+                return null;
+            }
+
+            var genericType = key.DeclaringType.GetGenericTypeDefinition();
+
+            var typeArgs = key.IsGenericMethod && !key.IsGenericMethodDefinition
+                ? key.GetGenericArguments()
+                : null;
+
+            var bindingFlags = (key.IsStatic ? BindingFlags.Static : BindingFlags.Instance) |
+                               (key.IsPublic ? BindingFlags.Public : BindingFlags.NonPublic);
+
+            var genericTypeKey = genericType.GetMethods(bindingFlags)
+                .FirstOrDefault(method => method.Name == key.Name && ArgsMatch(key.GetParameters(), method, typeArgs));
+
+            if (genericTypeKey != null)
+            {
+                IMethodCallTranslator translator;
+
+                if (Registry.TryGetValue(genericTypeKey, out translator))
+                {
+                    return translator;
+                }
+
+                if (genericTypeKey.IsGenericMethod && !genericTypeKey.IsGenericMethodDefinition)
+                {
+                    if (Registry.TryGetValue(genericTypeKey.GetGenericMethodDefinition(), out translator))
+                    {
+                        return translator;
+                    }
+                }
+            }
+
             return null;
+        }
+
+        /// <summary>
+        /// Checks all interfaces that implement the key method to see if one of them has an IMethodCallTranslator defined
+        /// </summary>
+        /// <param name="key">MethodInfo to test.  Must be an instance method on a class.</param>
+        /// <returns>Null if no IMethodCallTranslator is found</returns>
+        protected virtual IMethodCallTranslator GetItemFromInterfaces(MethodInfo key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+            
+            if (key.IsStatic || (key.DeclaringType == null) || (!key.DeclaringType.IsClass))
+            {
+                return null;
+            }
+
+            foreach (var interfaceType in key.DeclaringType.GetInterfaces())
+            {
+                var interfaceMap = key.DeclaringType.GetInterfaceMap(interfaceType);
+
+                var interfaceMethod = interfaceMap.TargetMethods
+                    .Where(p => p == key)
+                    .Select((p, i) => interfaceMap.InterfaceMethods[i])
+                    .FirstOrDefault();
+
+                if (interfaceMethod != null)
+                {
+                    var translator = GetItem(interfaceMethod);
+                    if (translator != null)
+                    {
+                        return translator;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private bool ArgsMatch(ParameterInfo[] args, MethodInfo method, Type[] typeArgs)
+        {
+            if (!method.IsGenericMethodDefinition && (typeArgs != null))
+            {
+                return false;
+            }
+
+            if (method.IsGenericMethodDefinition)
+            {
+                if ((typeArgs == null) || (typeArgs.Length != method.GetGenericArguments().Length))
+                {
+                    return false;
+                }
+
+                method = method.MakeGenericMethod(typeArgs);
+            }
+
+            var methodArgs = method.GetParameters();
+            if (methodArgs.Length != args.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                if (!methodArgs[i].ParameterType.IsAssignableFrom(args[i].ParameterType))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
     }
---
+++ Src/Couchbase.Linq/QueryGeneration/MethodCallTranslators/ListIndexMethodCallTranslator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couchbase.Linq.QueryGeneration.MethodCallTranslators
+{
+    class ListIndexMethodCallTranslator : IMethodCallTranslator
+    {
+        private static readonly MethodInfo[] SupportedMethodsStatic = {
+            typeof (IList).GetMethod("get_Item"),
+            typeof (IList<>).GetMethod("get_Item")
+        };
+
+        public IEnumerable<MethodInfo> SupportMethods
+        {
+            get
+            {
+                return SupportedMethodsStatic;
+            }
+        }
+
+        public Expression Translate(MethodCallExpression methodCallExpression, N1QlExpressionTreeVisitor expressionTreeVisitor)
+        {
+            if (methodCallExpression == null)
+            {
+                throw new ArgumentNullException("methodCallExpression");
+            }
+
+            var expression = expressionTreeVisitor.Expression;
+
+            expressionTreeVisitor.VisitExpression(methodCallExpression.Object);
+            expression.Append('[');
+            expressionTreeVisitor.VisitExpression(methodCallExpression.Arguments[0]);
+            expression.Append(']');
+
+            return methodCallExpression;
+        }
+    }
+}
