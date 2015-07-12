72ac6c60cec714307533444e56aee1a9d26b1b05
Brant Burnett <bburnett@centeredgesoftware.com>
2015-07-12 18:50:02 -0400

    Support for advanced string operations
    Motivation
    ----------
    Support advanced string operations such as string comparisons, splitting,
    substrings, trim, and capitalization.
    
    Modifications
    -------------
    Added method call translators for a variety of System.String methods
    Added StringComparisonExpression to assist with string.Compare calls
    Added escaping to string constants that contain a single-quote (')
    Added support for constant expressions of type char
    
    Results
    -------
    Supports the following System.String Methods:
    string.Compare(x, y) < 0, etc
    string.CompareTo(x) < 0, etc
    string.IndexOf(x)
    string.Length
    string.Replace(oldValue, newValue)
    string.Split()
    string.Split(chars)
    string.Substring(index)
    string.Substring(index, length)
    string[index]
    string.Trim()
    string.Trim(chars)
    string.TrimStart()
    string.TrimStart(chars)
    string.TrimEnd()
    string.TrimEnd(chars)
    string.ToUpper()
    string.ToLower()
    
    Improved compatibility when including string and character constants in
    queries

---
+++ Src/Couchbase.Linq/QueryGeneration/MethodCallTranslators/StringTrimMethodCallTranslator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couchbase.Linq.QueryGeneration.MethodCallTranslators
+{
+    class StringTrimMethodCallTranslator : IMethodCallTranslator
+    {
+        private static readonly MethodInfo[] SupportedMethodsStatic =
+        {
+            typeof (string).GetMethod("Trim", Type.EmptyTypes),
+            typeof (string).GetMethod("Trim", new[] { typeof (char[]) }),
+            typeof (string).GetMethod("TrimStart"),
+            typeof (string).GetMethod("TrimEnd")
+        };
+
+        public IEnumerable<MethodInfo> SupportMethods
+        {
+            get
+            {
+                return SupportedMethodsStatic;
+            }
+        }
+
+        public Expression Translate(MethodCallExpression methodCallExpression, N1QlExpressionTreeVisitor expressionTreeVisitor)
+        {
+            if (methodCallExpression == null)
+            {
+                throw new ArgumentNullException("methodCallExpression");
+            }
+
+            var expression = expressionTreeVisitor.Expression;
+
+            expression.Append(methodCallExpression.Method.Name == "TrimStart" ? "LTRIM(" :
+                methodCallExpression.Method.Name == "TrimEnd" ? "RTRIM(" : "TRIM(");
+            expressionTreeVisitor.VisitExpression(methodCallExpression.Object);
+
+            if (methodCallExpression.Arguments.Count > 0)
+            {
+                if (methodCallExpression.Arguments[0].Type != typeof (char[]))
+                {
+                    throw new NotSupportedException("String Trim Operations Expect Character Array Parameters");
+                }
+
+                try
+                {
+                    var lambda = Expression.Lambda<Func<char[]>>(methodCallExpression.Arguments[0]).Compile();
+                    var chars = lambda.Invoke();
+
+                    if ((chars != null) && (chars.Length > 0))
+                    {
+                        expression.Append(", ");
+
+                        expressionTreeVisitor.VisitExpression(Expression.Constant(new String(chars), typeof (string)));
+                    }
+                }
+                catch (NotSupportedException ex)
+                {
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    throw new NotSupportedException("Unable To Parse Trim Character Set.  Dynamic Expressions Are Not Supported", ex);
+                }
+            }
+
+            expression.Append(")");
+
+            return methodCallExpression;
+        }
+    }
+}
---
+++ Src/Couchbase.Linq/QueryGeneration/MethodCallTranslators/SubstringMethodCallTranslator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couchbase.Linq.QueryGeneration.MethodCallTranslators
+{
+    class SubstringMethodCallTranslator : IMethodCallTranslator
+    {
+        private static readonly MethodInfo[] SupportedMethodsStatic =
+        {
+            typeof (string).GetMethod("Substring", new[] { typeof (int) }),
+            typeof (string).GetMethod("Substring", new[] { typeof (int), typeof(int) }),
+            typeof (string).GetMethod("get_Chars", new[] { typeof (int) })
+        };
+
+        public IEnumerable<MethodInfo> SupportMethods
+        {
+            get
+            {
+                return SupportedMethodsStatic;
+            }
+        }
+
+        public Expression Translate(MethodCallExpression methodCallExpression, N1QlExpressionTreeVisitor expressionTreeVisitor)
+        {
+            if (methodCallExpression == null)
+            {
+                throw new ArgumentNullException("methodCallExpression");
+            }
+
+            var expression = expressionTreeVisitor.Expression;
+
+            expression.Append("SUBSTR(");
+            expressionTreeVisitor.VisitExpression(methodCallExpression.Object);
+            expression.Append(", ");
+            expressionTreeVisitor.VisitExpression(methodCallExpression.Arguments[0]);
+
+            if (methodCallExpression.Arguments.Count > 1)
+            {
+                expression.Append(", ");
+                expressionTreeVisitor.VisitExpression(methodCallExpression.Arguments[1]);
+            }
+            else if (methodCallExpression.Method.Name == "get_Chars")
+            {
+                // Called str[i], so return a single character at i
+                expression.Append(", 1");
+            }
+
+            expression.Append(")");
+
+            return methodCallExpression;
+        }
+    }
+}
