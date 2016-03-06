907b515bedb2e05740bade184f5de9e629c11ab1
Brant Burnett <bburnett@centeredgesoftware.com>
2016-03-05 21:41:33 -0500

    Improved query exception handling
    Motivation
    ----------
    Current exception handling during query execution can be unclear, and is
    difficult to trap with filtered catch blocks.
    
    Modifications
    -------------
    All errors during query execution are thrown as a CouchbaseQueryException.
    
    If the Errors collection is present and populated, include it in the
    Errors property of the CouchbaseQueryException.  If there is only one
    error, its message becomes the exception message.  If there is more than
    one error, a generic error message is used that refers the developer to
    the Errors property.
    
    If an Exception is returned instead, a generic error message is used and
    the Exception becomes the InnerException of the CouchbaseQueryException.
    
    If neither Errors or Exception is populated (which should not happen
    unless there is a bug further up the line), the message indicates that
    there was an unknown error.
    
    Results
    -------
    Exceptions can be easily trapped by catching CouchbaseQueryException
    specifically.  If trapped, the entire Errors collection is available in a
    property, as is the Exception in the InnerException property.  The
    exception message is more readable and informative.

---
+++ Src/Couchbase.Linq/CouchbaseQueryException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Couchbase.N1QL;
+
+namespace Couchbase.Linq
+{
+    /// <summary>
+    /// Thrown if an error occurs during the execution of a LINQ N1QL query.
+    /// </summary>
+    public class CouchbaseQueryException : ApplicationException
+    {
+        private readonly ReadOnlyCollection<Error> _errors;
+
+        /// <summary>
+        /// Errors returned by the Couchbase Server, if any.
+        /// </summary>
+        public ReadOnlyCollection<Error> Errors
+        {
+            get { return _errors; }
+        }
+
+        internal CouchbaseQueryException(string message) :
+            this(message, (Exception) null)
+        {
+        }
+
+        internal CouchbaseQueryException(string message, Exception innerException) :
+            base(message, innerException)
+        {
+            _errors = new ReadOnlyCollection<Error>(new Error[] {});
+        }
+
+        internal CouchbaseQueryException(string message, IList<Error> errors) :
+            base(message)
+        {
+            _errors = new ReadOnlyCollection<Error>(errors);
+        }
+    }
+}
