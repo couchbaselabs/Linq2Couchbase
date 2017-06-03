6a106f9b7477e08b41aff9d6fe61c1aa73bc7605
Brant Burnett <bburnett@centeredgesoftware.com>
2017-06-03 16:24:24 -0400

    Issue #199: Add option to suppress document filters
    Motivation
    ----------
    When performing LEFT JOINs, filters which are automatically injected via
    the DocumentFilterManager can produce unexpected behaviors.  This is due
    to a N1QL limitation that you cannot apply filters before a JOIN, only
    after the JOIN is complete.
    
    Modifications
    -------------
    Added BucketContext.Query overload that allows the user to suppress the
    filters, such as the DocumentTypeFilter.
    
    Added unit tests for all variants to ensure proper behavior.
    
    Results
    -------
    When performing a LEFT JOIN, the user can apply
    BucketQueryOptions.SuppressFilters to the right hand side of the join.
    This will prevent the LINQ expressions which cannot be translated to
    N1QL from being applied to the query.

--- Src/Couchbase.Linq/IBucketContext.cs
+++ Src/Couchbase.Linq/IBucketContext.cs
@@ -55,6 +55,15 @@ namespace Couchbase.Linq
         IQueryable<T> Query<T>();
 
         /// <summary>
+        /// Queries the current <see cref="IBucket" /> for entities of type T. This is the target of
+        /// a LINQ query and requires that the associated JSON document have a type property that is the same as T.
+        /// </summary>
+        /// <typeparam name="T">An entity or POCO representing the object graph of a JSON document.</typeparam>
+        /// <param name="options">Options to control the returned query.</param>
+        /// <returns><see cref="IQueryable{T}" /> which can be used to query the bucket.</returns>
+        IQueryable<T> Query<T>(BucketQueryOptions options);
+
+        /// <summary>
         /// Saves the specified document.
         /// </summary>
         /// <typeparam name="T"></typeparam>
