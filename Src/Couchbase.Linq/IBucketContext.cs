97c3a1f21ba21ead4c6ccb3335d75548c3a0eb2a
Brant Burnett <bburnett@centeredgesoftware.com>
2020-06-02 11:45:04 -0400

    Issue #289: Remove change tracking
    Motivation
    ----------
    In order to keep the scope of the SDK 3 update under control, we should
    temporarily remove change tracking. We don't believe this function is
    extensively used, and it can be added back later based on feedback.
    
    Modifications
    -------------
    Remove Castle.Core and dynamic proxy generation.
    
    Remove change tracking interfaces, methods, and classes.
    
    Remove exceptions related to change tracking.
    
    Drop System.ComponentModel.Annotations dependency from the make SDK,
    but keep it on test libraries so the `[Key]` annotations don't need to
    be removed from test POCOs.
    
    Results
    -------
    Greatly simplified scope for conversion to SDK 3.

--- Src/Couchbase.Linq/IBucketContext.cs
+++ Src/Couchbase.Linq/IBucketContext.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using Couchbase.Configuration.Client;
 using Couchbase.Core;
-using Couchbase.N1QL;
 
 namespace Couchbase.Linq
 {
@@ -26,27 +25,6 @@ namespace Couchbase.Linq
         ClientConfiguration Configuration { get; }
 
         /// <summary>
-        /// Begins change tracking for the current request. To complete and save the changes call <see cref="SubmitChanges()"/>.
-        /// </summary>
-        void BeginChangeTracking();
-
-        /// <summary>
-        /// Ends change tracking on the current context.
-        /// </summary>
-        void EndChangeTracking();
-
-        /// <summary>
-        /// Submits the changes.
-        /// </summary>
-        void SubmitChanges();
-
-        /// <summary>
-        /// Submits the changes with options.
-        /// </summary>
-        /// <param name="options">Options to control how changes are submitted.</param>
-        void SubmitChanges(SaveOptions options);
-
-        /// <summary>
         /// Queries the current <see cref="IBucket" /> for entities of type T. This is the target of
         /// a LINQ query and requires that the associated JSON document have a type property that is the same as T.
         /// </summary>
@@ -62,50 +40,5 @@ namespace Couchbase.Linq
         /// <param name="options">Options to control the returned query.</param>
         /// <returns><see cref="IQueryable{T}" /> which can be used to query the bucket.</returns>
         IQueryable<T> Query<T>(BucketQueryOptions options);
-
-        /// <summary>
-        /// Saves the specified document.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="document">The document.</param>
-        void Save<T>(T document);
-
-        /// <summary>
-        /// Removes the specified document.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="document">The document.</param>
-        void Remove<T>(T document);
-
-        /// <summary>
-        /// If true, generate change tracking proxies for documents during deserialization. Defaults to false for higher performance queries.
-        /// </summary>
-        bool ChangeTrackingEnabled { get; }
-
-        #region Mutation State
-
-        /// <summary>
-        /// The current <see cref="N1QL.MutationState"/>.  May return null if there
-        /// have been no mutations.
-        /// </summary>
-        /// <remarks>
-        /// This value is updated as mutations are applied via <see cref="Save{T}"/>.  It may be used
-        /// to enable read-your-own-write by passing the value to <see cref="Extensions.QueryExtensions.ConsistentWith{T}"/>.
-        /// If you are using change tracking, this value won't be valid until after a call to <see cref="SubmitChanges()"/>.
-        /// This function is only supported on Couchbase Server 4.5 or later.
-        /// </remarks>
-        MutationState MutationState { get; }
-
-        /// <summary>
-        /// Resets the <see cref="MutationState"/> to start a new set of mutations.
-        /// </summary>
-        /// <remarks>
-        /// If you are using an <see cref="IBucketContext"/> over and extended period of time,
-        /// performing a reset regularly is recommend.  This will help keep the size of the
-        /// <see cref="MutationState"/> to a minimum.
-        /// </remarks>
-        void ResetMutationState();
-
-        #endregion
     }
 }
