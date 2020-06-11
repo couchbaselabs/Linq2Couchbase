14b9724dfb3657beb1fe1f716c25792693000520
Brant Burnett <bburnett@centeredgesoftware.com>
2020-06-11 09:17:27 -0400

    Add AsAsyncEnumerable and ToListAsync
    Motivation
    ----------
    Make it easier for consumers to work with IQueryable asynchronously.
    
    Modifications
    -------------
    Implement IQueryable extension methods which work around casting the
    IQueryable to an IAsyncEnumerable.
    
    Results
    -------
    Easy usage!
    
    Relates to #281

--- Src/Couchbase.Linq/Extensions/QueryExtensions.cs
+++ Src/Couchbase.Linq/Extensions/QueryExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 using Couchbase.Linq.Execution;
 
 namespace Couchbase.Linq.Extensions
@@ -13,6 +14,33 @@ namespace Couchbase.Linq.Extensions
     /// </summary>
     public static partial class QueryExtensions
     {
+        /// <summary>
+        /// Returns an <see cref="IAsyncEnumerable{T}"/> for an <see cref="IQueryable{T}"/> to allow async enumeration.
+        /// </summary>
+        /// <typeparam name="T">Type of item to query.</typeparam>
+        /// <param name="source">Source <see cref="IQueryable{T}"/></param>.
+        /// <returns>An <see cref="IAsyncEnumerable{T}"/> which can be enumerated asynchronously.</returns>
+        /// <remarks>
+        /// This method stops subsequent methods in the chain from being applied to any generated query.
+        /// For example, a subsequent call to Queryable.Where to apply a predicate would be applied in-memory
+        /// to the query results rather than adding the predicate to the generated query.
+        /// </remarks>
+        /// <exception cref="InvalidOperationException">The implementation of <see cref="IQueryable{T}"/> does not also implement <see cref="IAsyncEnumerable{T}"/>.</exception>
+        public static IAsyncEnumerable<T> AsAsyncEnumerable<T>(this IQueryable<T> source) =>
+            source as IAsyncEnumerable<T> ?? throw new InvalidOperationException("The implementation of IQueryable<T> does not also implement IAsyncEnumerable<T>.");
+
+        /// <summary>
+        /// Executes an <see cref="IQueryable{T}"/> asynchronously and returns a list of results.
+        /// </summary>
+        /// <typeparam name="T">Type of item to query.</typeparam>
+        /// <param name="source">Source <see cref="IQueryable{T}"/></param>.
+        /// <param name="cancellationToken">An optional <see cref="CancellationToken"/>.</param>
+        /// <returns>A list of results.</returns>
+        /// <exception cref="InvalidOperationException">The implementation of <see cref="IQueryable{T}"/> does not also implement <see cref="IAsyncEnumerable{T}"/>.</exception>
+        public static ValueTask<List<T>> ToListAsync<T>(this IQueryable<T> source,
+            CancellationToken cancellationToken = default) =>
+            source.AsAsyncEnumerable().ToListAsync(cancellationToken);
+
         private static TResult ExecuteAsync<TSource, TResult>(
             MethodInfo operatorMethodInfo,
             IQueryable<TSource> source,
