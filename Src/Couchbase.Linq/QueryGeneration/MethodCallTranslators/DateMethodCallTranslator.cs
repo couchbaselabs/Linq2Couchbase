6002dc8263546be53207b8df56cccfa8a7d0728b
gritse <24323376+gritse@users.noreply.github.com>
2024-08-07 21:54:22 +0200

    Add DateMethodCallTranslator to support DateTime.Date and DateTimeOffset.Date properties

---
+++ Src/Couchbase.Linq/QueryGeneration/MethodCallTranslators/DateMethodCallTranslator.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Couchbase.Linq.QueryGeneration.MethodCallTranslators;
+
+internal class DateMethodCallTranslator : IMethodCallTranslator
+{
+    private static readonly MethodInfo[] SupportedMethodsStatic =
+    {
+        typeof (DateTime).GetMethod("get_Date"),
+        typeof (DateTimeOffset).GetMethod("get_Date")
+    };
+
+    public IEnumerable<MethodInfo> SupportMethods
+    {
+        get
+        {
+            return SupportedMethodsStatic;
+        }
+    }
+
+    public Expression Translate(MethodCallExpression methodCallExpression, N1QlExpressionTreeVisitor expressionTreeVisitor)
+    {
+        if (methodCallExpression == null)
+        {
+            throw new ArgumentNullException("methodCallExpression");
+        }
+
+        var expression = expressionTreeVisitor.Expression;
+
+        expression.Append("DATE_TRUNC_STR(");
+        expressionTreeVisitor.Visit(methodCallExpression.Object);
+        expression.Append(",");
+        expression.Append(@"""day""");
+        expression.Append(")");
+
+        return methodCallExpression;
+    }
+}
